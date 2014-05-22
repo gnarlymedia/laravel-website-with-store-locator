de7112c6b5cdd582ec2df876beac857e8d75acb3
GnarlyMedia <simon@gnarlymedia.com.au>
2014-05-22 15:38:30 +1000

    WEB-5, continue bootstrap alignment of elements

--- app/views/splash.blade.php
+++ app/views/splash.blade.php
@@ -8,7 +8,7 @@
 	<img src="/assets/images/background/background_1600.jpg" alt="background" width="1600" height="805" id="background-image" />
 	
 	<div id="patients_referrers">
-		<img src="/assets/images/logo/capital_radiology.png" alt="capital_radiology" width="240" height="76" id="logo" />
+		<img src="/assets/images/logo/capital_radiology.png" alt="capital_radiology" width="240" height="76" id="logo" class="img-responsive"/>
 		<a href="/theme/home" id="referrer_button" width="265" height="77"></a>
 		<img src="/assets/images/elements/vertical_line.png" alt="vertical_line" width="1" height="215" id="vertical_line" />
 		<a href="/patients-home" id="patient_button"></a>
@@ -17,15 +17,17 @@
    	<img src="/assets/images/elements/glyphicon.png" alt="glyphicon" width="50" height="50" id="glyphicon" />
     
 	<div class="row">
-	    <div id="social" class="col-md-9 col-sm-9 col-xs-9">
-			<a href="#"><img src="/assets/images/social/Sample-1.pdf-1_03.jpg" alt="Sample-1.pdf-1_03" width="97" height="83" /></a>
-			<a href="#"><img src="/assets/images/social/Sample-1.pdf-1_04.jpg" alt="Sample-1.pdf-1_04" width="104" height="83" /></a>
-			<a href="#"><img src="/assets/images/social/Sample-1.pdf-1_05.jpg" alt="Sample-1.pdf-1_05" width="105" height="83" /></a>
-			<a href="#"><img src="/assets/images/social/Sample-1.pdf-1_06.jpg" alt="Sample-1.pdf-1_06" width="100" height="83" /></a>
-			<a href="#"><img src="/assets/images/social/Sample-1.pdf-1_07.jpg" alt="Sample-1.pdf-1_07" width="104" height="83" /></a>
+    	<div class="col-md-1 col-sm-1 col-xs-1"></div>
+	    <div id="social" class="col-md-8 col-sm-8 col-xs-8">
+			<a href="#"><img src="/assets/images/social/Sample-1.pdf-1_03.jpg" alt="Sample-1.pdf-1_03" width="97" height="83" class="img-responsive" /></a>
+			<a href="#"><img src="/assets/images/social/Sample-1.pdf-1_04.jpg" alt="Sample-1.pdf-1_04" width="104" height="83" class="img-responsive" /></a>
+			<a href="#"><img src="/assets/images/social/Sample-1.pdf-1_05.jpg" alt="Sample-1.pdf-1_05" width="105" height="83" class="img-responsive"/></a>
+			<a href="#"><img src="/assets/images/social/Sample-1.pdf-1_06.jpg" alt="Sample-1.pdf-1_06" width="100" height="83" class="img-responsive"/></a>
+			<a href="#"><img src="/assets/images/social/Sample-1.pdf-1_07.jpg" alt="Sample-1.pdf-1_07" width="104" height="83" class="img-responsive"/></a>
 		</div>
-		<div id="copyright" class="col-md-3 col-sm-3 col-xs-3">
+		<div id="copyright" class="col-md-2 col-sm-2 col-xs-2">
 			Copyright (©) 2011-2014 Capital Radiology
 		</div>
+    	<div class="col-md-1 col-sm-1 col-xs-1"></div>
 	</div>
 @stop
