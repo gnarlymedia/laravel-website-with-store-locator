99117c53ba4eb722bdb5956f79a31cc5e8b95482
GnarlyMedia <simon@gnarlymedia.com.au>
2014-05-26 15:16:55 +1000

    WEB-5, add social icons

--- app/views/layouts/mockup.blade.php
+++ app/views/layouts/mockup.blade.php
@@ -67,6 +67,21 @@
 
 	@yield('content', 'This content could not be found')
 
+	<div class="row">
+    	<div class="col-md-1 col-sm-1 col-xs-1"></div>
+	    <div id="social" class="col-md-6 col-sm-6 col-xs-12">
+			<a href="#"><img src="/assets/images/social/Sample-1.pdf-1_03.jpg" alt="Sample-1.pdf-1_03" width="97" height="83" class="img-responsive" /></a>
+			<a href="#"><img src="/assets/images/social/Sample-1.pdf-1_04.jpg" alt="Sample-1.pdf-1_04" width="104" height="83" class="img-responsive" /></a>
+			<a href="#"><img src="/assets/images/social/Sample-1.pdf-1_05.jpg" alt="Sample-1.pdf-1_05" width="105" height="83" class="img-responsive"/></a>
+			<a href="#"><img src="/assets/images/social/Sample-1.pdf-1_06.jpg" alt="Sample-1.pdf-1_06" width="100" height="83" class="img-responsive"/></a>
+			<a href="#"><img src="/assets/images/social/Sample-1.pdf-1_07.jpg" alt="Sample-1.pdf-1_07" width="104" height="83" class="img-responsive"/></a>
+		</div>
+		<div id="copyright" class="col-md-4 col-sm-4 col-xs-12">
+			Copyright (©) 2011-2014 Capital Radiology
+		</div>
+    	<div class="col-md-1 col-sm-1 col-xs-1"></div>
+	</div>
+
     </div> <!-- /container -->
 
 
--- app/views/mockup-referrer.blade.php
+++ app/views/mockup-referrer.blade.php
@@ -13,25 +13,27 @@
 @stop
 
 @section('content')
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<img src="/assets/images/iStock_2_smaller.jpg" alt="iStock_2" width="400" height="267" id="referrer-body-image"/><h1>Results Direct to your Desk</h1>
-<p>In addition to fax and secure email Capital Radiology offers an efficient, reliable and convenient way of delivering reports and digital images direct to your practice management software. Alternatively you can access your patient reports and images over the Internet.</p>
-<p>To access this service please contact support@capitalradiology.com.au</p>
-
-<h2>Benefits to referrers</h2>
-<ul><li>View images on same screen as reports within patient record (Medical Director & Best Practice)</li>
-<li>For other Practice Management Software simply click a hyperlink within the report and load the images from the web portal</li>
-<li>Access images and reports anywhere anytime from the web portal</li>
-<li>Discuss a significant finding with one of our Radiologists within minutes of reporting</li>
-<li>Eliminate film storage problems</li>
-<li>Eliminate inconvenience of patients losing or forgetting films</li>
-<li>Forward patient’s reports and images for second opinion quickly, easily and securely</li>
-<li>Review historical reports with images (not retrospectively)</li>
-</ul>
-<h2>Benefits to your patients</h2>
-<ul>
-<li>No waiting or returning to collect films and reports</li>
-<li>No film storage or lost records</li>
-</ul>
+	<p>&nbsp;</p>
+	<p>&nbsp;</p>
+	<img src="/assets/images/iStock_2_smaller.jpg" alt="iStock_2" width="400" height="267" id="referrer-body-image"/>
+	<h1>Results Direct to your Desk</h1>
+	<p>In addition to fax and secure email Capital Radiology offers an efficient, reliable and convenient way of delivering reports and digital images direct to your practice management software. Alternatively you can access your patient reports and images over the Internet.</p>
+	<p>To access this service please contact support@capitalradiology.com.au</p>
+	
+	<h2>Benefits to referrers</h2>
+	<ul>
+		<li>View images on same screen as reports within patient record (Medical Director & Best Practice)</li>
+		<li>For other Practice Management Software simply click a hyperlink within the report and load the images from the web portal</li>
+		<li>Access images and reports anywhere anytime from the web portal</li>
+		<li>Discuss a significant finding with one of our Radiologists within minutes of reporting</li>
+		<li>Eliminate film storage problems</li>
+		<li>Eliminate inconvenience of patients losing or forgetting films</li>
+		<li>Forward patient’s reports and images for second opinion quickly, easily and securely</li>
+		<li>Review historical reports with images (not retrospectively)</li>
+	</ul>
+	<h2>Benefits to your patients</h2>
+	<ul>
+		<li>No waiting or returning to collect films and reports</li>
+		<li>No film storage or lost records</li>
+	</ul>
 @stop
