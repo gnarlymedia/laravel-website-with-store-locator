7e7d8023cc7d9fad505e3ac6fbe1beea43212419
GnarlyMedia <simon@gnarlymedia.com.au>
2014-06-10 10:00:29 +1000

    WEB-9, removed blog image directories from repo, added blog database seeder command

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder {
         $this->call('PatientTableSeeder');
         $this->call('ModalityTableSeeder');
         $this->call('MarkersTableSeeder');
+        $this->call('PostTableFakeSeeder');
 	}
 }
 
